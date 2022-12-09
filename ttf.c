8580f4d2a574409b8f3d01619ae8416519d1386e
Nathan Houghton <nathan@brainwerk.org>
2022-12-09 09:47:19 -0800

    Replace tabs with spaces

--- ttf.c
+++ ttf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2009 Nathaniel Houghton <nathan@brainwerk.org>
+ * Copyright (c) 2009,2022 Nathaniel Houghton <nathan@brainwerk.org>
  *
  * Permission to use, copy, modify, and distribute this software for
  * any purpose with or without fee is hereby granted, provided that
@@ -28,12 +28,12 @@
 #define NUM_COL 5
 
 struct options {
-	double duration;
-	double low_freq;
-	double high_freq;
-	double freq_gap;
-	double char_gap;
-	double f_s;
+    double duration;
+    double low_freq;
+    double high_freq;
+    double freq_gap;
+    double char_gap;
+    double f_s;
 };
 
 void output_char(struct options *, char);
@@ -315,108 +315,108 @@ static uint8_t char_map[][NUM_ROW][NUM_COL] = {
 int
 main(int argc, char **argv)
 {
-	struct options opt;
-	char *str;
-	int ch;
-	int n;
+    struct options opt;
+    char *str;
+    int ch;
+    int n;
 
-	opt.f_s = 44100;
-	opt.low_freq = 400;
-	opt.high_freq = 1100;
-	opt.char_gap = 0.1;
-	opt.duration = 0.4;
+    opt.f_s = 44100;
+    opt.low_freq = 400;
+    opt.high_freq = 1100;
+    opt.char_gap = 0.1;
+    opt.duration = 0.4;
 
-	while ((ch = getopt(argc, argv, "l:h:r:d:g:")) != -1) {
-		switch (ch) {
-		case 'l':
-			opt.low_freq = atof(optarg);
-			break;
-		case 'h':
-			opt.high_freq = atof(optarg);
-			break;
-		case 'r':
-			opt.f_s = atof(optarg);
-			break;
-		case 'd':
-			opt.duration = atof(optarg);
-			break;
-		case 'g':
-			opt.char_gap = atof(optarg);
-			break;
-		default:
-			usage();
-			/* NOTREACHED */
-		}
-	}
-	argc -= optind;
-	argv += optind;
+    while ((ch = getopt(argc, argv, "l:h:r:d:g:")) != -1) {
+        switch (ch) {
+        case 'l':
+            opt.low_freq = atof(optarg);
+            break;
+        case 'h':
+            opt.high_freq = atof(optarg);
+            break;
+        case 'r':
+            opt.f_s = atof(optarg);
+            break;
+        case 'd':
+            opt.duration = atof(optarg);
+            break;
+        case 'g':
+            opt.char_gap = atof(optarg);
+            break;
+        default:
+            usage();
+            /* NOTREACHED */
+        }
+    }
+    argc -= optind;
+    argv += optind;
 
-	if (!*argv)
-		usage();
+    if (!*argv)
+        usage();
 
-	str = *argv;
+    str = *argv;
 
-	opt.freq_gap = (opt.high_freq - opt.low_freq) / NUM_ROW;
+    opt.freq_gap = (opt.high_freq - opt.low_freq) / NUM_ROW;
 
-	while (*str) {
-		output_char(&opt, *str);
-		for (n = 0; n < opt.f_s * opt.char_gap; ++n) {
-			int16_t val = 0;
+    while (*str) {
+        output_char(&opt, *str);
+        for (n = 0; n < opt.f_s * opt.char_gap; ++n) {
+            int16_t val = 0;
 
-			fwrite(&val, sizeof(val), 1, stdout);
-		}
+            fwrite(&val, sizeof(val), 1, stdout);
+        }
 
-		++str;
-	}
+        ++str;
+    }
 
-	return 0;
+    return 0;
 }
 
 void
 output_char(struct options *opt, char c)
 {
-	uint8_t ind;
-	int n;
-	int16_t val;
-	double freq;
-	int cur_col;
-	int i;
+    uint8_t ind;
+    int n;
+    int16_t val;
+    double freq;
+    int cur_col;
+    int i;
 
-	c = tolower(c);
+    c = tolower(c);
 
-	if (c >= 'a' && c <= 'z')
-		ind = c - 'a';
-	else
-		ind = 'z' - 'a' + 1;
+    if (c >= 'a' && c <= 'z')
+        ind = c - 'a';
+    else
+        ind = 'z' - 'a' + 1;
 
-	for (n = 0; n < opt->f_s * opt->duration; ++n) {
-		cur_col = round(n / (opt->f_s * opt->duration /
-		    (NUM_COL - 1)));
+    for (n = 0; n < opt->f_s * opt->duration; ++n) {
+        cur_col = round(n / (opt->f_s * opt->duration /
+            (NUM_COL - 1)));
 
-		val = 0;
+        val = 0;
 
-		for (i = NUM_ROW - 1; i >= 0; --i) {
-			if (char_map[ind][i][cur_col] == 0)
-				continue;
+        for (i = NUM_ROW - 1; i >= 0; --i) {
+            if (char_map[ind][i][cur_col] == 0)
+                continue;
 
-			freq = opt->high_freq - (opt->freq_gap * i);
+            freq = opt->high_freq - (opt->freq_gap * i);
 
-			val += AMP * cos(2  * M_PI * freq * n /
-			    opt->f_s);
-		}
+            val += AMP * cos(2  * M_PI * freq * n /
+                opt->f_s);
+        }
 
-		fwrite(&val, sizeof(val), 1, stdout);
-	}
+        fwrite(&val, sizeof(val), 1, stdout);
+    }
 }
 
 void
 usage(void)
 {
-	extern char *__progname;
+    extern char *__progname;
 
-	fprintf(stderr, "usage: %s [-l low_freq (Hz)] "
-	    "[-h high_freq (Hz)] [-r sampling_rate "
-	    "(Hz)] [-d character_duration (sec)] [-g "
-	    "character gap (sec)] string\n", __progname);
-	exit(1);
+    fprintf(stderr, "usage: %s [-l low_freq (Hz)] "
+        "[-h high_freq (Hz)] [-r sampling_rate "
+        "(Hz)] [-d character_duration (sec)] [-g "
+        "character gap (sec)] string\n", __progname);
+    exit(1);
 }
